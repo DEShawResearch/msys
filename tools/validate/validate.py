31967a5fb9ed439f3a103e21b839914924c75d8f
Justin Gullingsrud <gullingj@deshaw.com>
2011-10-20 14:03:16 -0500

    A few more validation checks.
    [git-p4: depot-paths = "//depot/desrad/main/sw/libs/msys/": change = 123441]

--- tools/validate/validate.py
+++ tools/validate/validate.py
@@ -8,9 +8,32 @@ class TestCompatibility(UT.TestCase):
         self.mol=_mol
 
     def testContiguousGids(self):
-        ''' Ensure that gids are contiguous, starting at zero.  '''
+        ''' Gids must be contiguous, starting at zero '''
         gids=[a.gid for a in self.mol.atoms]
         self.assertEqual(gids, range(self.mol.natoms))
+
+class TestAnton(UT.TestCase):
+    def setUp(self):
+        self.mol=_mol
+
+    def testHasConstraints(self):
+        ''' The system must have constraints '''
+        constraints=[t for t in self.mol.tables if t.category=='constraint']
+        self.assertTrue(len(constraints)>0)
+
+class TestSanity(UT.TestCase):
+    def setUp(self):
+        self.mol=_mol
+
+    def testConsistentMasses(self):
+        ''' Particles with equal atomic number must have equal mass '''
+        m=dict()
+        for a in self.mol.atoms:
+            m.setdefault(a.atomic_number, set()).add(a.mass)
+        for anum, masses in m.items():
+            self.assertEqual(len(masses), 1, 
+                    "Multiple masses for atomic number %s: \n\t%s" % (
+                        anum, list(masses)))
 
 def Validate(mol):
     global _mol
