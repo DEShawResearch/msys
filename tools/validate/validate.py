84fc3a0ec84eb145ea6d19071c0efdf519302a97
Justin Gullingsrud <gullingj@deshaw.com>
2011-10-20 14:35:35 -0500

    more validation
    [git-p4: depot-paths = "//depot/desrad/main/sw/libs/msys/": change = 123448]

--- tools/validate/validate.py
+++ tools/validate/validate.py
@@ -3,16 +3,7 @@ import unittest as UT
 
 _mol = None
 
-class TestCompatibility(UT.TestCase):
-    def setUp(self): 
-        self.mol=_mol
-
-    def testContiguousGids(self):
-        ''' Gids must be contiguous, starting at zero '''
-        gids=[a.gid for a in self.mol.atoms]
-        self.assertEqual(gids, range(self.mol.natoms))
-
-class TestAnton(UT.TestCase):
+class TestStrict(UT.TestCase):
     def setUp(self):
         self.mol=_mol
 
@@ -20,10 +11,6 @@ class TestAnton(UT.TestCase):
         ''' The system must have constraints '''
         constraints=[t for t in self.mol.tables if t.category=='constraint']
         self.assertTrue(len(constraints)>0)
-
-class TestSanity(UT.TestCase):
-    def setUp(self):
-        self.mol=_mol
 
     def testConsistentMasses(self):
         ''' Particles with equal atomic number must have equal mass '''
@@ -34,6 +21,26 @@ class TestSanity(UT.TestCase):
             self.assertEqual(len(masses), 1, 
                     "Multiple masses for atomic number %s: \n\t%s" % (
                         anum, list(masses)))
+
+class TestBasic(UT.TestCase):
+    def setUp(self):
+        self.mol=_mol
+
+    def testContiguousGids(self):
+        ''' Gids must be contiguous, starting at zero '''
+        gids=[a.gid for a in self.mol.atoms]
+        self.assertEqual(gids, range(self.mol.natoms))
+
+    def testHasNonbonded(self):
+        ''' Every particle must have a nonbonded param assignment '''
+        self.assertTrue('nonbonded' in self.mol.table_names)
+        nb=self.mol.table('nonbonded')
+        for t in nb.terms:
+            self.assertFalse(t.param is None,
+                    "Particle with gid %d has no nonbonded parameters" %
+                    t.atom(0).gid)
+
+
 
 def Validate(mol):
     global _mol
