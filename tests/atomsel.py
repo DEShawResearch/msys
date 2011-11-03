cdee2d9093bb57a680c5f47ed950fa5695ffbfce
Justin Gullingsrud <gullingj@deshaw.com>
2011-11-03 14:54:32 -0500

    Use 'atomicnumber 1' instead of hydrogen when comparing to VMD because  VMD's hydrogen atom selection is based on the atom name.
    [git-p4: depot-paths = "//depot/desrad/main/sw/libs/msys/": change = 124659]

--- tests/atomsel.py
+++ tests/atomsel.py
@@ -27,12 +27,16 @@ def compare_atomsel(coord_ent,sel):
     #for g in ent_gids: 
       #if g not in vmd_gids:
         #print g,names[g], al[g].pos[:2]
+    return False
   else:
     print "match for [%s] (%d) "% (sel, len(vmd_gids))
+    return True
 
 DMSROOT='/proj/desres/root/Linux/x86_64/dms_inputs/1.5.5/share'
 coord_vmd=molecule.load("dms","%s/2f4k.dms" % DMSROOT)
 coord_ent = msys.LoadDMS("%s/2f4k.dms" % DMSROOT,True)
+#coord_vmd=molecule.load("dms","/tmp/pro.dms")
+#coord_ent = msys.LoadDMS("/tmp/pro.dms",True)
 
 compare_atomsel(coord_ent,"all")
 compare_atomsel(coord_ent,"none")
@@ -41,24 +45,30 @@ compare_atomsel(coord_ent,"gid 10 20 30")
 compare_atomsel(coord_ent,"resid 1 to 20 22 24 to 27 35")
 compare_atomsel(coord_ent,"protein")
 compare_atomsel(coord_ent,"backbone")
-compare_atomsel(coord_ent,"hydrogen")
+#compare_atomsel(coord_ent,"hydrogen")
+compare_atomsel(coord_ent,"atomicnumber 1")
+
+compare_atomsel(coord_ent,"chain A B")
+#compare_atomsel(coord_ent,"segid L14")
+
 compare_atomsel(coord_ent,"water")
 compare_atomsel(coord_ent,"x<3")
 compare_atomsel(coord_ent,"y>=1")
 compare_atomsel(coord_ent,"z<=-5")
 
 compare_atomsel(coord_ent,"pbwithin 3 of protein")
-compare_atomsel(coord_ent,"noh and pbwithin 3 of protein")
+compare_atomsel(coord_ent,"(not atomicnumber 1) and pbwithin 3 of protein")
 compare_atomsel(coord_ent,"backbone and pbwithin 4 of water")
 
 compare_atomsel(coord_ent,"same residue as within 4 of protein")
-compare_atomsel(coord_ent,"noh and within 1.5 of protein")
-compare_atomsel(coord_ent,"noh and within 2.5 of protein")
-compare_atomsel(coord_ent,"noh and within 5.5 of protein")
+compare_atomsel(coord_ent,"(not atomicnumber 1) and within 1.5 of protein")
+compare_atomsel(coord_ent,"(not atomicnumber 1) and within 2.5 of protein")
+compare_atomsel(coord_ent,"(not atomicnumber 1) and within 5.5 of protein")
 compare_atomsel(coord_ent,"within 0 of protein")
 compare_atomsel(coord_ent,"withinbonds 2 of name C CA N O")
 compare_atomsel(coord_ent,"fragment 0")
 compare_atomsel(coord_ent,"fragment 1")
+
 
 compare_atomsel(coord_ent,"sqrt(sqr(x)+sqr(y))<5")
 compare_atomsel(coord_ent,"abs(x-y)<5")
