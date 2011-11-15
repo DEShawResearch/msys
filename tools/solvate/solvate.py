5c5e2d2e461844af048f9e22bc3db17a1ef6d013
Justin Gullingsrud <gullingj@deshaw.com>
2011-11-15 14:08:50 -0500

    Overhaul of dms-solvate.  Got rid of thickness-based box specification;  now specification is of absolute box size.  Added ability to provide  one's own water box, crucial for various usability reasons.
    [git-p4: depot-paths = "//depot/desrad/main/sw/libs/msys/": change = 125466]

--- tools/solvate/solvate.py
+++ tools/solvate/solvate.py
@@ -4,27 +4,26 @@ import os, msys
 WAT=os.path.join(os.getenv('MSYS_PREFIX'), 'share', 'solvate', 'h2o.dms')
 WATRAD = 2.4
 
-def Solvate(mol, thickness=0, minmax=None,
-            xpad=None, ypad=None, zpad=None, 
-            chain='WT', verbose=False,
-            cubic=False):
+def Solvate(mol, watbox=None, dims=None, center=None,
+        chain='WT', verbose=False):
     '''
     Build and return a new system consisting of mol plus solvent.
 
-    If no solute is provided, the solute is treated as a point at
-    the origin.  thickness specifies the amount of solute added along
-    each coordinate axis; this can be overridden by xpad, ypad, zpad.
-    The extent of the solvent can also be given explicitly with minmax.
-    If cubic is true, the box size will be expanded to the size of the
-    longest dimension.
+    If no box dimensions are provided, the box size along each axis will
+    be equal to the greater of that of the input structure and the input
+    water box.
 
     Return the solvated system; no modifications are made to the input system.
     '''
 
     mol=mol.clone()
+    if watbox is None: watbox=WAT
 
-    wat=msys.LoadDMS(WAT)
+    if verbose:
+        print "Loading water box from '%s'" % watbox
+    wat=msys.LoadDMS(watbox)
     watsize=[wat.cell[i][i] for i in range(3)]
+    molsize=[mol.cell[i][i] for i in range(3)]
 
     # find a chain name for the waters that doesn't overlap with the 
     # input structure.
@@ -35,70 +34,58 @@ def Solvate(mol, thickness=0, minmax=None,
     for c in wat.chains:
         c.name = watchain
 
-    if verbose: print "finding bounding box..."
-    if len(mol.atoms):
-        first=mol.atoms[0].pos
-        xmin, ymin, zmin = first
-        xmax, ymax, zmax = first
-        for a in mol.atoms:
-            x,y,z = a.pos
-            if   x<xmin: xmin=x
-            elif x>xmax: xmax=x
-            if   y<ymin: ymin=y
-            elif y>ymax: ymax=y
-            if   z<zmin: zmin=z
-            elif z>zmax: zmax=z
+    if verbose:
+        print "Dims specified as '%s'" % dims
+    if dims is None:
+        dims = [max(x) for x in zip(watsize, molsize)]
+    elif len(dims)==1:
+        dims=[float(dims[0])]*3
+    elif len(dims)!=3:
+        raise ValueError, "Dims must be given as list of one or three values"
     else:
-        xmin, ymin, zmin =[wat.cell[i][i] for i in range(3)]
-        xmin=ymin=zmin=-t
-        xmax=ymax=zmax= t/2-1e15
-    smin=[xmin,ymin,zmin]
-    smax=[xmax,ymax,zmax]
+        dims=[float(x) for x in dims]
     
-    # The definition of the solvent range is a little tricky.
-    # For each direction, if pad is given, then the range is the
-    # solute minmax along that axis padded by the specified amount,
-    # no matter what thickness and minmax are.  If pad is not given,
-    # then the minmax argument is checke first, and if it's also missing,
-    # then we use thickness.  
-    pad=(xpad, ypad, zpad)
-    for i in range(3):
-        t=thickness
-        if pad[i] is None:
-            if minmax:
-                smin[i]=minmax[0][i]
-                smax[i]=minmax[1][i]
-            else:
-                smin[i] -= t
-                smax[i] += t
-        else:
-            t=pad[i]
-            smin[i] -= t
-            smax[i] += t
+    if center is None:
+        center=[0,0,0]
+    elif len(center)!=3:
+        raise ValueError, "Center must be given as list of three values"
+    else:
+        center=[float(x) for x in center]
 
-    if cubic:
-        delta=0.5*max(smax[i]-smin[i] for i in range(3))
-        center=[0.5*(smax[i]+smin[i]) for i in range(3)]
-        smin=[center[i]-delta for i in range(3)]
-        smax=[center[i]+delta for i in range(3)]
+    if center==[0,0,0]:
+        WITHIN='within'
+    else:
+        WITHIN='pbwithin'
+
+
+    # update the cell
+    mol.cell[0][:]=[dims[0],0,0]
+    mol.cell[1][:]=[0,dims[1],0]
+    mol.cell[2][:]=[0,0,dims[2]]
 
     # extract where to put the water
-    xmin, ymin, zmin = smin
-    xmax, ymax, zmax = smax
-    dx, dy, dz = [a-b for a,b in zip(smax,smin)]
-    nx = int(dx/watsize[0]) + 1
-    ny = int(dy/watsize[1]) + 1
-    nz = int(dz/watsize[2]) + 1
+    xmin = center[0]-0.5*dims[0]
+    ymin = center[1]-0.5*dims[1]
+    zmin = center[2]-0.5*dims[2]
+    xmax = center[0]+0.5*dims[0]
+    ymax = center[1]+0.5*dims[1]
+    zmax = center[2]+0.5*dims[2]
+    nx = int(dims[0]/watsize[0]) + 1
+    ny = int(dims[1]/watsize[1]) + 1
+    nz = int(dims[2]/watsize[2]) + 1
+
+    xshift = -0.5 * (nx-1)*watsize[0]
+    yshift = -0.5 * (ny-1)*watsize[1]
+    zshift = -0.5 * (nz-1)*watsize[2]
 
     # replicate the template water box
     if verbose: print "replicating %d x %d x %d" % (nx,ny,nz)
     for i in range(nx):
-        xdelta = xmin + i*watsize[0]
+        xdelta = xshift + i*watsize[0]
         for j in range(ny):
-            ydelta = ymin + j*watsize[1]
+            ydelta = yshift + j*watsize[1]
             for k in range(nz):
-                zdelta = zmin + k*watsize[2]
-
+                zdelta = zshift + k*watsize[2]
                 newatoms = mol.append(wat)
                 for a in newatoms:
                     a.x += xdelta
@@ -106,25 +93,27 @@ def Solvate(mol, thickness=0, minmax=None,
                     a.z += zdelta
 
     if verbose: print "removing overlaps"
-    # FIXME: use the low-level interface for speed
-    mol = mol.clone(
-        'not (chain %s and same residue as (x<%f or y<%f or z<%f or x>%f or y>%f or z>%f or within %f of (not chain %s)))' % (
-            watchain,xmin,ymin,zmin,xmax,ymax,zmax,WATRAD, watchain))
 
-    # assign the water chain name
+    toofar='x<%s or y<%s or z<%s or x>%s or y>%s or z>%s' % (
+            xmin,ymin,zmin,xmax,ymax,zmax)
+
+    toonear='%s %s of not chain %s' % (WITHIN, WATRAD, watchain)
+    mol=mol.clone(
+            'not same residue as (chain %s and oxygen and (%s or %s))' % (
+                watchain, toofar, toonear))
+
+    # assign the water chain name and water resids
+    watres = 1
     for c in mol.chains:
         if c.name == watchain:
             c.name = chain
+            for r in c.residues:
+                r.resid = watres
+                watres += 1
 
     mol.reassignGids()
 
-    if verbose: print "updating global cell to (%g %g %g)" % (
-        xmax-xmin, ymax-ymin, zmax-zmin)
-
-    # update the cell
-    mol.cell[0][:]=[xmax-xmin,0,0]
-    mol.cell[1][:]=[0,ymax-ymin,0]
-    mol.cell[2][:]=[0,0,zmax-zmin]
+    if verbose: print "updating global cell to (%g %g %g)" % tuple(dims)
 
     return mol
 
