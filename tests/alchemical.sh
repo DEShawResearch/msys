bb6aba45b6e4c69dd4e494cca794d813fd7fb1ea
Justin Gullingsrud <gullingj@deshaw.com>
2011-11-29 10:50:20 -0500

    Make the dms comparison more robust in the alchemical test.
    [git-p4: depot-paths = "//depot/desrad/main/sw/libs/msys/": change = 126312]

--- tests/alchemical.sh
+++ tests/alchemical.sh
@@ -10,27 +10,32 @@ TMP=/tmp
 set -e
 DUMPOPTS="--without-provenance --reorder --without-forcefield"
 
-for DIR in $DESMOND_MAEFF_INPUT_PATH/fep/*
+canonicalize() {
+    sqlite3 $1 "update alchemical_stretch_harm_term set p0=p1,p1=p0 where p0>p1"
+    sqlite3 $1 "update alchemical_angle_harm_term set p0=p2,p2=p0 where p0>p2"
+    sqlite3 $1 "update alchemical_dihedral_trig_term set p0=p3,p1=p2,p2=p1,p3=p0 where p1>p2"
+    sqlite3 $1 "update alchemical_pair_12_6_es_term set p0=p1,p1=p0 where p0>p1"
+}
+
+
+for DIR in $DESMOND_MAEFF_INPUT_PATH/fep/fep*
 #for DIR in $DESMOND_MAEFF_INPUT_PATH/fep/fep1
 do
     echo "Reading A.mae, B.mae, and atom.map from $DIR"
     
     # viparr1 route.
     mae2dms $DIR/C.mae $TMP/viparr.dms
-    sqlite3 /tmp/viparr.dms "update alchemical_dihedral_trig_term set p0=p3,p1=p2,p2=p1,p3=p0 where p1>p2"
-    sqlite3 /tmp/viparr.dms "update alchemical_pair_12_6_es_term set p0=p1,p1=p0 where p0>p1"
-
+    canonicalize $TMP/viparr.dms
     dms-dump $DUMPOPTS $TMP/viparr.dms | grep -v "\-----" > $TMP/viparr.dump
     
     # msys route.
     mae2dms $DIR/A.mae $TMP/A.dms
     mae2dms $DIR/B.mae $TMP/B.dms
     dms-alchemical $TMP/A.dms $TMP/B.dms $DIR/atom.map $TMP/msys.dms
-    sqlite3 /tmp/msys.dms "update alchemical_dihedral_trig_term set p0=p3,p1=p2,p2=p1,p3=p0 where p1>p2"
-    sqlite3 /tmp/msys.dms "update alchemical_pair_12_6_es_term set p0=p1,p1=p0 where p0>p1"
+    canonicalize $TMP/msys.dms
+    dms-dump $DUMPOPTS $TMP/msys.dms | grep -v "\-----" > $TMP/msys.dump
 
     # compare
-    dms-dump $DUMPOPTS $TMP/msys.dms | grep -v "\-----" > $TMP/msys.dump
     ndiff -relerr 1e-4 -silent $TMP/viparr.dump $TMP/msys.dump
 
     echo "OK"
