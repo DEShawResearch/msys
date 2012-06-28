3001f6e58b5b9917d5c1643e966f0a37814f4e57
Justin Gullingsrud <gullingj@deshaw.com>
2012-06-28 11:09:37 -0500

    Use SystemImporter for reading pdb files.
    [git-p4: depot-paths = "//depot/desrad/main/sw/libs/msys/": change = 139483]

--- src/pdb/pdb.cxx
+++ src/pdb/pdb.cxx
@@ -24,16 +24,12 @@ SystemPtr desres::msys::ImportPDB( std::string const& path ) {
 
     char pdbstr[PDB_BUFFER_LENGTH];
 
-    typedef std::map<std::string,Id> ResMap;
-    ResMap resmap;
-    Id chn = BadId;
-    Id res = BadId;
-
     FILE* fd = fopen(path.c_str(), "r");
     if (!fd) throw std::runtime_error("Failed opening pdb file");
     boost::shared_ptr<FILE> defer_close(fd, fclose);
 
     SystemPtr mol = System::create();
+    SystemImporter imp(mol);
 
     int indx=PDB_EOF;
     do {
@@ -54,29 +50,7 @@ SystemPtr desres::msys::ImportPDB( std::string const& path ) {
             strip_whitespace(segid);
             resid = atoi(residstr);
 
-            /* Start a new chain if the chain name changes */
-            if (bad(chn) || mol->chain(chn).name!=chainname) {
-                chn = mol->addChain();
-                mol->chain(chn).name = chainname;
-                res = BadId;
-            }
-            /* Start a new residue if the resname or resid changes */
-            if (bad(res) || mol->residue(res).resid!=resid
-                    || mol->residue(res).name!=resname) {
-                /* First see if this residue is in our hash */
-                char key[64];
-                sprintf(key, "%s - %s - %d", chainname, resname, resid);
-                ResMap::const_iterator it=resmap.find(key);
-                if (it==resmap.end()) {
-                    res = mol->addResidue(chn);
-                    mol->residue(res).resid = resid;
-                    mol->residue(res).name = resname;
-                    resmap[key]=res;
-                }
-            }
-            /* Append particles to the end of the residue */
-            Id atm = mol->addAtom(res);
-            mol->atom(atm).name = name;
+            Id atm = imp.addAtom(chainname, segid, resid, resname, name);
             mol->atom(atm).x = x;
             mol->atom(atm).y = y;
             mol->atom(atm).z = z;
