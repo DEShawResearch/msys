6014258ff8f1fadadc1cae26bf5d3fa11068001c
Justin Gullingsrud <gullingj@deshaw.com>
2012-07-02 15:33:25 -0500

    Missing file.
    [git-p4: depot-paths = "//depot/desrad/main/sw/libs/msys/": change = 139645]

---
+++ tests/test_param_index.cxx
@@ -0,0 +1,29 @@
+#include "dms.hxx"
+#include <cstdio>
+
+using namespace desres::msys;
+
+int main(int argc, char *argv[]) {
+    if (argc<5) {
+        fprintf(stderr, "Usage: %s input.dms tablename property value\n",
+                argv[0]);
+        return 1;
+    }
+    SystemPtr sys = ImportDMS(argv[1]);
+    TermTablePtr table = sys->table(argv[2]);
+    ParamTablePtr params = table->params();
+    Id col = params->propIndex(argv[3]);
+    for (int i=4; i<argc; i++) {
+        IdList ids;
+        switch (params->propType(col)) {
+            case IntType: ids=params->findInt(col, atoi(argv[i])); break;
+            case FloatType: ids=params->findFloat(col, atof(argv[i])); break;
+            default:
+            case StringType: ids=params->findString(col, argv[i]); break;
+        }
+        printf("found %4lu params with %s value %s\n", 
+                ids.size(), argv[3], argv[i]);
+    }
+    return 0;
+}
+
