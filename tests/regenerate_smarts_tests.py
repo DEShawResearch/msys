23d27c99b56d97d874cac3f100e24dad511f0321
Zhou Fan <fanzho@deshaw.com>
2013-03-12 15:42:00 -0500

    Add script to regenerate SMARTS matching baselines for unit test
    [git-p4: depot-paths = "//depot/desrad/main/sw/libs/msys/": change = 162410]

---
+++ tests/regenerate_smarts_tests.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2.7
+
+import sys
+import os
+TMPDIR=os.getenv('TMPDIR', 'objs/Linux/x86_64')
+sys.path.insert(0,os.path.join(TMPDIR, 'lib', 'python'))
+import msys
+import json
+
+mols = [msys.Load('/proj/desres/root/Linux/x86_64/dms_inputs/1.5.4/share/ww.dms'), msys.Load('/proj/desres/root/Linux/x86_64/dms_inputs/1.5.4/share/membrane.dms')]
+annot_mols = []
+atoms = []
+matches = []
+for mol in mols:
+    msys.AssignBondOrderAndFormalCharge(mol)
+    annot_mols.append(msys.AnnotatedSystem(mol))
+    atoms.append(mol.select('not water'))
+    matches.append([])
+
+f = open('tests/smarts_tests.json', 'r')
+d = json.loads(f.read())
+f.close()
+for patt in d['smarts']:
+    sp = msys.SmartsPattern(patt)
+    for i in range(2):
+        matches[i].append(sp.findMatches(annot_mols[i], atoms[i]))
+
+d['ww_matches'] = matches[0]
+d['membrane_matches'] = matches[1]
+f = open('tests/smarts_tests.json', 'w')
+f.write(json.dumps(d))
+f.close()
