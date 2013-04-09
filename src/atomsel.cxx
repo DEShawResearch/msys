650a186f7357d111085a37b588e26ef346cf56d5
Justin Gullingsrud <gullingj@deshaw.com>
2013-04-09 15:20:55 -0500

    Now that we don't depend on an antlr runtime, there's no real need for  atom selection support to be a configuration option.
    [git-p4: depot-paths = "//depot/desrad/main/sw/libs/msys/": change = 163580]

--- src/atomsel.cxx
+++ src/atomsel.cxx
@@ -1,14 +1,10 @@
 #include "atomsel.hxx"
-
-#ifndef MSYS_WITHOUT_ATOMSEL
 #include "atomsel/vmd.hxx"
 #include "atomsel/msys_keyword.hxx"
-#endif
 
 namespace desres { namespace msys { 
 
     IdList Atomselect(SystemPtr ptr, const std::string& txt) {
-#ifndef MSYS_WITHOUT_ATOMSEL
         atomsel::VMD vmd(ptr);
         atomsel::PredicatePtr result = vmd.parse(txt);
         if (!result) {
@@ -17,9 +13,6 @@ namespace desres { namespace msys {
         atomsel::Selection s(atomsel::full_selection(ptr));
         result->eval(s);
         return s.ids();
-#else
-        MSYS_FAIL("No atom selection support");
-#endif
     }
 
 }}
