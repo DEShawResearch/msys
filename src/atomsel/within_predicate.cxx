6ea6207cc9e5babb100f6e2e0f54104932b41a48
Justin Gullingsrud <gullingj@deshaw.com>
2014-04-17 10:15:33 -0500

    More within revamps.  Fixed 'nearest' logic for the case when fewer        than k atoms are available.  Speedup in nearest by avoiding        duplicate bounding box search.
    [git-p4: depot-paths = "//depot/desrad/main/sw/libs/msys/": change = 181329]

--- src/atomsel/within_predicate.cxx
+++ src/atomsel/within_predicate.cxx
@@ -27,6 +27,45 @@ struct point_t {
     point_t(double _x, double _y, double _z) : x(_x), y(_y), z(_z) {}
 };
 
+struct posarray : boost::noncopyable {
+    std::vector<point_t> pos;
+    double xmin, ymin, zmin;
+    double xmax, ymax, zmax;
+    Id size;
+
+    posarray(SystemPtr mol, Selection const& S)
+    : xmin(), ymin(), zmin(), xmax(), ymax(), zmax(), size(S.count()) {
+        pos.resize(size);
+        for (Id i=0, j=0, n=S.size(); i<n; i++) {
+            if (S[i]) {
+                double x = mol->atomFAST(i).x;
+                double y = mol->atomFAST(i).y;
+                double z = mol->atomFAST(i).z;
+                if (j==0) {
+                    xmin = xmax = x;
+                    ymin = ymax = y;
+                    zmin = zmax = z;
+                } else {
+                    xmin = std::min(xmin, x);
+                    ymin = std::min(ymin, y);
+                    zmin = std::min(zmin, z);
+                    xmax = std::max(xmax, x);
+                    ymax = std::max(ymax, y);
+                    zmax = std::max(zmax, z);
+                }
+                pos[j].x = x;
+                pos[j].y = y;
+                pos[j].z = z;
+                ++j;
+            }
+        }
+    }
+    double const& x(Id i) const { return pos[i].x; }
+    double const& y(Id i) const { return pos[i].y; }
+    double const& z(Id i) const { return pos[i].z; }
+};
+
+
 struct voxel_t {
     /* For voxels on the edge of the grid, compute and store the
      * indices of neighbor voxels.  For other voxels, the neighbors
@@ -56,52 +95,6 @@ struct voxel_t {
       points[num++] = p;
   }
 };
-
-
-/* find the bounding box of selected atoms. Return 0 if no atoms 
- * selected. */
-static
-int find_bbox(const Selection& S, const point_t* points, 
-              double *min, double *max) {
-  double xmin, ymin, zmin;
-  double xmax, ymax, zmax;
-
-  // find first selected atom
-  int i,n=S.size();
-  for (i=0; i<n; i++) if (S[i]) break;
-  if (i==n) return 0;
-
-  // initialize min/max to first atom position
-  xmin=xmax=points[i].x;
-  ymin=ymax=points[i].y;
-  zmin=zmax=points[i].z;
-
-  // extend minmax
-  for (;i<n; i++) {
-    if (!S[i]) continue;
-    point_t const& p = points[i];
-    double x=p.x;
-    double y=p.y;
-    double z=p.z;
-
-    if (x<xmin) xmin=x;
-    if (x>xmax) xmax=x;
-
-    if (y<ymin) ymin=y;
-    if (y>ymax) ymax=y;
-
-    if (z<zmin) zmin=z;
-    if (z>zmax) zmax=z;
-  }
-  // store to supplied buffers
-  min[0]=xmin;
-  min[1]=ymin;
-  min[2]=zmin;
-  max[0]=xmax;
-  max[1]=ymax;
-  max[2]=zmax;
-  return 1;
-}
 
 static
 void find_voxel_full_shell_neighbors(voxel_t *mesh, int nx, int ny, int nz) {
@@ -190,30 +183,12 @@ namespace {
   };
 }
 
-static void find_within( const point_t* points,
+static void find_within( const point_t* wat,
+                         posarray const& pro,
                          Selection& S,
                          Selection const& subsel,
                          double rad,
                          bool exclude ) {
-
-  double min[3], max[3];
-  double xmin, ymin, zmin;
-  double xsize, ysize, zsize;
-  const double r2 = rad*rad;
-  double ir;
-  voxel_t * mesh;
-
-  int nx, ny, nz, nvoxel;
-  int i;
-
-  double t0=1000*now();
-
-  /* find bounding box of subselection */
-  if (!find_bbox(subsel, points, min, max)) {
-    /* no atoms in subsel */
-    S.clear();
-    return;
-  }
 
   double t1=1000*now();
 
@@ -227,39 +202,31 @@ static void find_within( const point_t* points,
     S.intersect(subsel);
     return;
   }
-  ir = 1.0/rad;
+  const double ir = 1.0/rad;
+  const double r2 = rad*rad;
 
   /* extend bounding box by selection radius */
-  for (i=0; i<3; i++) {
-    min[i] -= rad;
-    max[i] += rad;
-  }
-  xmin=min[0];
-  ymin=min[1];
-  zmin=min[2];
-  xsize=max[0]-xmin;
-  ysize=max[1]-ymin;
-  zsize=max[2]-zmin;
+  double xmin = pro.xmin - rad;
+  double ymin = pro.ymin - rad;
+  double zmin = pro.zmin - rad;
+  double xsize = rad + pro.xmax - xmin;
+  double ysize = rad + pro.ymax - ymin;
+  double zsize = rad + pro.zmax - zmin;
 
   /* create and initialize voxel mesh */
-  nx = (int)(xsize/rad)+1;
-  ny = (int)(ysize/rad)+1;
-  nz = (int)(zsize/rad)+1;
-  nvoxel = nx*ny*nz;
+  int nx = (int)(xsize/rad)+1;
+  int ny = (int)(ysize/rad)+1;
+  int nz = (int)(zsize/rad)+1;
+  int nvoxel = nx*ny*nz;
 
   double t2=1000*now();
 
-  mesh = new voxel_t[nvoxel];
-
-
-  /* map subsel atoms to voxels */
-  for (Id i=0; i<subsel.size(); i++) {
-    if (!subsel[i]) continue;
-
-    point_t const& p = points[i];
-    int xi = (p.x-xmin)*ir;
-    int yi = (p.y-ymin)*ir;
-    int zi = (p.z-zmin)*ir;
+  /* map pro atoms to voxels */
+  voxel_t * mesh = new voxel_t[nvoxel];
+  for (Id i=0, n=pro.size; i<n; i++) {
+    int xi = (pro.x(i)-xmin)*ir;
+    int yi = (pro.y(i)-ymin)*ir;
+    int zi = (pro.z(i)-zmin)*ir;
     int index = xi + nx*(yi + ny*zi);
     mesh[index].add(i);
   }
@@ -272,32 +239,28 @@ static void find_within( const point_t* points,
 
   double t4=1000*now();
 
-  /* loop over atoms in left selection */
+  /* loop over wat atoms */
   for (Id i=0; i<S.size(); i++) {
-    int xi,yi,zi;
-    int j, index;
-    int n_nbrs;
-    const int * nbrs;
-    int self;
-    int on;
-    voxel_t * v;
     if (!S[i]) continue;
     if (subsel[i]) continue;
-    point_t const& p = points[i];
+    point_t const& p = wat[i];
     const double x = p.x;
     const double y = p.y;
     const double z = p.z;
-    xi = (x-xmin)*ir;
-    yi = (y-ymin)*ir;
-    zi = (z-zmin)*ir;
+    int xi = (x-xmin)*ir;
+    int yi = (y-ymin)*ir;
+    int zi = (z-zmin)*ir;
     if (xi<0 || xi>=nx ||
         yi<0 || yi>=ny ||
         zi<0 || zi>=nz) {
       S[i]=0;
       continue;
     }
-    index = xi + nx*(yi + ny*zi);
-    v = mesh+index;
+    int index = xi + nx*(yi + ny*zi);
+    const voxel_t * v = mesh+index;
+    const int * nbrs;
+    int n_nbrs;
+    int self;
     if (v->nbrs) {
         /* edge voxel, use existing neighbors */
         nbrs = v->nbrs;
@@ -308,16 +271,15 @@ static void find_within( const point_t* points,
         n_nbrs = 27;
         self = index;
     }
-    on=0;
-    for (j=0; j<n_nbrs; j++) {
+    bool on=0;
+    for (int j=0; j<n_nbrs; j++) {
       const voxel_t* nbr = mesh + nbrs[j]+self;
-      int k, natoms = nbr->num;
-      for (k=0; k<natoms; k++) {
+      int natoms = nbr->num;
+      for (int k=0; k<natoms; k++) {
         const Id pk = nbr->points[k];
-        point_t const& p = points[pk];
-        double dx=x-p.x;
-        double dy=y-p.y;
-        double dz=z-p.z;
+        double dx=x-pro.x(pk);
+        double dy=y-pro.y(pk);
+        double dz=z-pro.z(pk);
         if (dx*dx + dy*dy + dz*dz <=r2) {
           on=1;
           break;
@@ -332,38 +294,46 @@ static void find_within( const point_t* points,
   delete [] mesh;
 
   double t6=1000*now();
-  if (0) printf("within %8.3f: tot %8.3f : %f %f %f %f %f %f\n", rad,t6-t0,
-          t1-t0,t2-t1,t3-t2,t4-t3,t5-t4,t6-t5);
+  if (0) printf("within %8.3f: tot %8.3f : %f %f %f %f %f\n", rad,t6-t1,
+          t2-t1,t3-t2,t4-t3,t5-t4,t6-t5);
 }
 
 void WithinPredicate::eval( Selection& S ) {
     Selection subsel = full_selection(sys);
     sub->eval(subsel);
 
-    std::vector<point_t> points(S.size());
-    for (unsigned i=0; i<S.size(); i++) {
-        if (S[i] || subsel[i]) {
-            points[i].x = sys->atom(i).x;
-            points[i].y = sys->atom(i).y;
-            points[i].z = sys->atom(i).z;
-        }
+    if (rad<=0) {
+        if (exclude) S.subtract(subsel);
+        S.intersect(subsel);
+        return;
     }
-    if (!points.size()) {
+
+    /* "protein" coordinates */
+    posarray pro(sys, subsel);
+    if (pro.size==0) {
         S.clear();
         return;
     }
 
+    /* "water" coordinates */
+    std::vector<point_t> wat(S.size());
+    for (unsigned i=0; i<S.size(); i++) {
+        if (S[i]) {
+            wat[i].x = sys->atomFAST(i).x;
+            wat[i].y = sys->atomFAST(i).y;
+            wat[i].z = sys->atomFAST(i).z;
+        }
+    }
+
     if (periodic) {
-        /* replicate the atoms in S within a bounding box around subsel */
-        double min[3], max[3];
-        if (!find_bbox(subsel, &points[0], min, max)) {
-            S.clear();
-            return;
-        }
-        for (int i=0; i<3; i++) {
-            min[i] -= rad;
-            max[i] += rad;
-        }
+        /* replicate the water within a bounding box around protein */
+        double xmin = pro.xmin - rad;
+        double ymin = pro.ymin - rad;
+        double zmin = pro.zmin - rad;
+        double xmax = pro.xmax + rad;
+        double ymax = pro.ymax + rad;
+        double zmax = pro.zmax + rad;
+
         IdList repids;  /* ids of replicated atoms */
         const double* A = sys->global_cell[0];
         const double* B = sys->global_cell[1];
@@ -378,26 +348,27 @@ void WithinPredicate::eval( Selection& S ) {
         double gc = C[2];
         for (Id id=0; id<S.size(); id++) {
             if (!S[id]) continue;
-            point_t p = points[id];
+            /* Need a copy since we'll append to wat below */
+            point_t p = wat[id];    
             for (int i=-1; i<=1; i++) {
                 double x = p.x + ga*i;
-                if (x<min[0] || x >= max[0]) continue;
+                if (x<xmin || x >= xmax) continue;
                 for (int j=-1; j<=1; j++) {
                     double y = p.y + gb*j;
-                    if (y<min[1] || y >= max[1]) continue;
+                    if (y<ymin || y >= ymax) continue;
                     for (int k=-1; k<=1; k++) {
                         double z = p.z + gc*k;
-                        if (z<min[2] || z >= max[2]) continue;
+                        if (z<zmin || z >= zmax) continue;
                         if (i==0 && j==0 && k==0) continue;
                         repids.push_back(id);
-                        points.push_back(point_t(x,y,z));
+                        wat.push_back(point_t(x,y,z));
                     }
                 }
             }
         }
 
-        Selection Srep(points.size());
-        Selection Ssub(points.size());
+        Selection Srep(wat.size());
+        Selection Ssub(wat.size());
         /* copy original flags */
         for (unsigned i=0; i<S.size(); i++) Srep[i]=S[i];
         /* turn on all replica flags - we know they're selected */
@@ -405,7 +376,7 @@ void WithinPredicate::eval( Selection& S ) {
         /* copy subsel into expanded set */
         for (unsigned i=0; i<subsel.size(); i++) Ssub[i]=subsel[i];
 
-        find_within( &points[0], Srep, Ssub, rad, false );
+        find_within( &wat[0], pro, Srep, Ssub, rad, false );
 
         /* copy Srep back into S */
         for (unsigned i=0; i<S.size(); i++) S[i]=Srep[i];
@@ -418,7 +389,7 @@ void WithinPredicate::eval( Selection& S ) {
         }
 
     } else { 
-        find_within( &points[0], S, subsel, rad, exclude );
+        find_within( &wat[0], pro, S, subsel, rad, exclude );
     }
 }
 
@@ -492,38 +463,31 @@ void KNearestPredicate::eval( Selection& S ) {
     Selection subsel = full_selection(_sys);
     _sub->eval(subsel);
 
-    /* empty subselection -> empty selection */
-    if (!subsel.size()) {
+    /* "protein" coordinates */
+    posarray pro(_sys, subsel);
+    if (pro.size==0) {
         S.clear();
         return;
     }
-    const Id subcnt = subsel.count();
-    //printf("sub cnt: %u\n", subcnt);
 
-    /* S.count() <= k: nothing to do */
-    const Id cnt = S.count();
-    //printf("S count: %u\n", cnt);
-    if (cnt<=_N) return;
+    /* we never include subselection */
+    S.subtract(subsel);
 
-    /* cache coordinates of interest */
+    /* more than k atoms available? */
+    double rmin = 0;
+    Selection smin(S);
+    Id nmin = smin.count();
+    if (nmin <= _N) return;
 
-    std::vector<point_t> points(S.size());
+    /* "water" coordinates */
+    std::vector<point_t> wat(S.size());
     for (unsigned i=0; i<S.size(); i++) {
-        if (S[i] || subsel[i]) {
-            points[i].x = _sys->atomFAST(i).x;
-            points[i].y = _sys->atomFAST(i).y;
-            points[i].z = _sys->atomFAST(i).z;
+        if (S[i]) {
+            wat[i].x = _sys->atomFAST(i).x;
+            wat[i].y = _sys->atomFAST(i).y;
+            wat[i].z = _sys->atomFAST(i).z;
         }
     }
-    if (!points.size()) {
-        S.clear();
-        return;
-    }
-
-    double rmin=0;
-    Selection smin(S);
-    find_within( &points[0], smin, subsel, 0, true);
-    Id nmin = smin.count();
 
     double rmax=2.5;
     Selection smax(0);
@@ -532,7 +496,7 @@ void KNearestPredicate::eval( Selection& S ) {
     /* increase rmax until Ny is at least _N */
     for (;;) {
         smax = S;
-        find_within( &points[0], smax, subsel, rmax, true);
+        find_within( &wat[0], pro, smax, subsel, rmax, false);
         nmax = smax.count();
         //printf("rmin %f nmin %u rmax %f nmax %u\n", rmin, nmin, rmax, nmax);
         if (nmax >= _N) break;
@@ -546,7 +510,7 @@ void KNearestPredicate::eval( Selection& S ) {
     for (int nb=0; nb<6; nb++) {
         Selection sm(S);
         double rm = 0.5*(rmin+rmax);
-        find_within( &points[0], sm, subsel, rm, true);
+        find_within( &wat[0], pro, sm, subsel, rm, false);
         Id nm = sm.count();
         //printf("rm %f nm %u\n", rm, nm);
         if (nm>=_N) {
@@ -562,36 +526,19 @@ void KNearestPredicate::eval( Selection& S ) {
     //printf("min: rad %f n %u\n", rmin, nmin);
     //printf("max: rad %f n %u\n", rmax, nmax);
 
-    /* cache the protein positions.
-     * FIXME: consider transpose for SIMD */
-    std::vector<double> pro;
-    pro.reserve(3*subcnt);
-    for (Id i=0, n=S.size(); i<n; i++) {
-        if (subsel[i]) {
-            const atom_t& atm = _sys->atomFAST(i);
-            pro.insert(pro.end(), &atm.x, &atm.x+3);
-        }
-    }
-
-    //printf("cached %lu pro positions\n", pro.size()/3);
-
     std::vector<std::pair<double,Id> > pts;
     /* for each water in smax but not in smin */
     for (Id i=0, n=S.size(); i<n; i++) {
         if (smin[i] || !smax[i]) continue;
+        point_t const& w = wat[i];
         double r2 = std::numeric_limits<double>::max();
-        double x = _sys->atomFAST(i).x;
-        double y = _sys->atomFAST(i).y;
-        double z = _sys->atomFAST(i).z;
         /* find min dist to protein */
-        const double* p = &pro[0];
-        for (Id j=0, m=subcnt; j<m; j++) {
-            double dx = x-p[0];
-            double dy = y-p[1];
-            double dz = z-p[2];
+        for (Id j=0, m=pro.size; j<m; j++) {
+            double dx = w.x-pro.x(j);
+            double dy = w.y-pro.y(j);
+            double dz = w.z-pro.z(j);
             double d2 = dx*dx + dy*dy + dz*dz;
             r2 = std::min(r2, d2);
-            p += 3;
         }
         pts.push_back(std::make_pair(r2, i));
     }
