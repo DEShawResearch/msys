a406610cbb3708df251f5b25b806aad7cd3a4189
Justin Gullingsrud <Justin.Gullingsrud@deshawresearch.com>
2014-12-03 15:25:49 -0500

    Update alchemical baselines.
    Change-Id: If4d4d5484c3b5e7e05f8af975c879d70e71dcf16

--- tests/alchemical.sh
+++ tests/alchemical.sh
@@ -3,30 +3,13 @@
 # exec desres-cleanenv \
 # -m ndiff/2.00-02/bin \
 # -m dms_inputs/1.5.8/share \
-# -m msys/1.6.2/bin \
+# -m msys/1.7.84/bin \
 # -- sh $0 "$@"
 #}
 
 TMP=/tmp
 set -e
 DUMPOPTS="--without-provenance --reorder --without-forcefield --without-paraminfo"
-
-add_insertion() {
-    sqlite3 $1 "alter table particle add column insertion varchar default ''"
-}
-
-canonicalize() {
-    set +e
-    sqlite3 $1 "update stretch_harm_term set p0=p1,p1=p0 where p0>p1"
-    sqlite3 $1 "update angle_harm_term set p0=p2,p2=p0 where p0>p2"
-    sqlite3 $1 "update dihedral_trig_term set p0=p3,p1=p2,p2=p1,p3=p0 where p1>p2"
-    sqlite3 $1 "update pair_12_6_es_term set p0=p1,p1=p0 where p0>p1"
-    sqlite3 $1 "update alchemical_stretch_harm_term set p0=p1,p1=p0 where p0>p1"
-    sqlite3 $1 "update alchemical_angle_harm_term set p0=p2,p2=p0 where p0>p2"
-    sqlite3 $1 "update alchemical_dihedral_trig_term set p0=p3,p1=p2,p2=p1,p3=p0 where p1>p2"
-    sqlite3 $1 "update alchemical_pair_12_6_es_term set p0=p1,p1=p0 where p0>p1"
-    set -e
-}
 
 for DIR in $DMS_INPUTS_PATH/fep/fep*
 #for DIR in $DESMOND_MAEFF_INPUT_PATH/fep/fep1
@@ -37,13 +20,10 @@ do
     
     # old msys route.
     dms-alchemical $DIR/A.dms $DIR/B.dms $DIR/atom.map $TMP/old.dms
-    canonicalize $TMP/old.dms
-    add_insertion $TMP/old.dms
     dms-dump $DUMPOPTS $TMP/old.dms | grep -v "\-----" > $TMP/old.dump
     
     # new msys route.
-    ./objs/Linux/x86_64/bin/dms-alchemical --keep-alchemical-noop $DIR/A.dms $DIR/B.dms $DIR/atom.map $TMP/new.dms
-    canonicalize $TMP/new.dms
+    ./objs/$DESRES_OS/$DESRES_ISA/bin/dms-alchemical $DIR/A.dms $DIR/B.dms $DIR/atom.map $TMP/new.dms
     dms-dump $DUMPOPTS $TMP/new.dms | grep -v "\-----" > $TMP/new.dump
 
     # compare
