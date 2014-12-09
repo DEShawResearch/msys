e3e9834ff905e4277732e07e4633da1ceccc2548
Justin Gullingsrud <Justin.Gullingsrud@deshawresearch.com>
2014-12-09 15:30:07 -0500

    Added --assign-type to dms2mae.
    DESRESCode#2124.
    
    Change-Id: If7dec1bf8fdd8f78f8c85f34363ed6632c697f36

--- tools/converters/dms2mae.cxx
+++ tools/converters/dms2mae.cxx
@@ -1,6 +1,7 @@
 #include "mae.hxx"
 #include "dms.hxx"
 #include <cstdio>
+#include <string.h>
 
 using namespace desres::msys;
 
@@ -13,22 +14,43 @@ static void usage(FILE *fd) {
 "\n"
 "Options:\n"
 "   -h, --help              show this help message and exit\n"
+"   -a, --assign-type       Reassign m_mmod_type\n"
 "\n"
 "dms2mae is the inverse of mae2dms, though with a more restricted\n"
 "range of supported forcefield terms.\n"
 );
 }
 
-int main(int argc, char *argv[]) {
-    if (argc==2 && argv[1][0]=='-' && argv[1][1]=='h') {
-        usage(stdout);
-        exit(0);
+static bool assign_type = false;
+
+static
+void parse_cmdline( int *pargc, char ***pargv ) {
+    int i,j=0;
+                           
+    for (i=0; i<pargc[0]; i++) {
+        pargv[0][j] = pargv[0][i];
+        if ( !strcmp(pargv[0][j], "-h") ||
+             !strcmp(pargv[0][j], "--help")) {
+            usage(stdout);
+            exit(0);
+        } else if ( !strcmp(pargv[0][j], "--assign-type") ||
+                    !strcmp(pargv[0][j], "-a")) {
+            assign_type = true;
+        }  else {
+            j++;
+        }
     }
+    pargc[0]=j;
+    pargv[0][j]=NULL;
+}
+int main(int argc, char *argv[]) {
+    parse_cmdline(&argc, &argv);
     if (argc!=3) {
         usage(stderr);
         exit(1);
     }
     SystemPtr h = ImportDMS(argv[1]);
+    if (assign_type) h->delAtomProp(h->atomPropIndex("m_mmod_type"));
     ExportMAE(h, argv[2], Provenance::fromArgs(argc,argv));
     return 0;
 }
