630215c4e3a2e1d7a3131b84685c75926d4e8ba1
Justin Gullingsrud <Justin.Gullingsrud@deshawresearch.com>
2015-01-19 15:26:33 -0500

    Added dcd reader.
    Change-Id: I488bcbe6d8de6eef6f8d8eb9d8212a98efabd5fb

--- src/molfile/libmolfile_plugin.h
+++ src/molfile/libmolfile_plugin.h
@@ -8,6 +8,8 @@ extern "C" {
 int msys_plugin_register(void *, vmdplugin_register_cb);
 int msys_dtrplugin_init(void);
 int msys_dtrplugin_register(void *, vmdplugin_register_cb);
+int msys_dcdplugin_init(void);
+int msys_dcdplugin_register(void *, vmdplugin_register_cb);
 
 #ifdef __cplusplus
 }
@@ -15,10 +17,12 @@ int msys_dtrplugin_register(void *, vmdplugin_register_cb);
 
 #define MOLFILE_INIT_ALL \
 msys_dtrplugin_init(); \
+msys_dcdplugin_init(); \
 
 #define MOLFILE_REGISTER_ALL(v,cb) \
 msys_plugin_register(v,cb); \
 msys_dtrplugin_register(v,cb); \
+msys_dcdplugin_register(v,cb); \
 
 #define MOLFILE_FINI_ALL \
 
