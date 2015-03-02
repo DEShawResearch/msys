242d1e4fb92393e481073a5e386332aecd0fc26a
Justin Gullingsrud <Justin.Gullingsrud@deshawresearch.com>
2015-03-02 16:08:22 -0500

    Bring back the dx plugin.
    Change-Id: I7e1cc066397b75fa7fd60344535a2069e33a6de8

--- src/molfile/libmolfile_plugin.c
+++ src/molfile/libmolfile_plugin.c
@@ -2,10 +2,12 @@
 
 int msys_plugin_init(void);
 int msys_plugin_register(void *, vmdplugin_register_cb);
-int msys_dtrplugin_init(void);
-int msys_dtrplugin_register(void *, vmdplugin_register_cb);
 int msys_dcdplugin_init(void);
 int msys_dcdplugin_register(void *, vmdplugin_register_cb);
+int msys_dtrplugin_init(void);
+int msys_dtrplugin_register(void *, vmdplugin_register_cb);
+int msys_dxplugin_init(void);
+int msys_dxplugin_register(void *, vmdplugin_register_cb);
 int msys_psfplugin_init(void);
 int msys_psfplugin_register(void *, vmdplugin_register_cb);
 int msys_xyzplugin_init(void);
@@ -13,16 +15,18 @@ int msys_xyzplugin_register(void *, vmdplugin_register_cb);
 
 void molfile_init_all(void) {
     msys_plugin_init();
-    msys_dtrplugin_init();
     msys_dcdplugin_init();
+    msys_dtrplugin_init();
+    msys_dxplugin_init();
     msys_psfplugin_init();
     msys_xyzplugin_init();
 }
 
 void molfile_register_all(void* v, vmdplugin_register_cb cb) {
     msys_plugin_register(v,cb);
-    msys_dtrplugin_register(v,cb);
     msys_dcdplugin_register(v,cb);
+    msys_dtrplugin_register(v,cb);
+    msys_dxplugin_register(v,cb);
     msys_psfplugin_register(v,cb);
     msys_xyzplugin_register(v,cb);
 }
