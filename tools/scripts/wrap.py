4d4c23cf1d3ab0fba69163ab3aee3153ad2456c0
Justin Gullingsrud <gullingj@deshawresearch.com>
2015-05-05 12:25:37 -0400

    Added --unrotate option to dms-frame.
    Change-Id: Ie4969b51053f104ac72e3c640c6cb4d977256b11

--- tools/scripts/wrap.py
+++ tools/scripts/wrap.py
@@ -3,7 +3,7 @@ import numpy as NP
 from msys.pfx import Pfx
 
 class Wrapper(object):
-    def __init__(self, system, center=None, glue=[]):
+    def __init__(self, system, center=None, glue=[], unrotate=False):
         mol = system
         pfx = Pfx(mol.topology, fixbonds=True)
         if isinstance(glue, str):
@@ -16,10 +16,17 @@ class Wrapper(object):
 
         self.mol = mol
         self.pfx = pfx
+        self.unrotate = unrotate
 
     def wrap(self):
         pos = self.mol.getPositions()
         box = self.mol.getCell()
+        if self.unrotate:
+            vecs = [r/NP.linalg.norm(r) for r in box]
+            rot = NP.array(vecs).T
+            pos = NP.dot(pos, rot)
+            box = NP.dot(box, rot)
+            self.mol.setCell(box)
         self.pfx.apply(pos,box)
         self.mol.setPositions(pos)
 
