ccad81f7f84f0062e93ae603949d2ef481dc9d08
Justin Gullingsrud <gullingj@deshawresearch.com>
2018-06-28 08:35:40 -0400

    1.7.232 - bump megamodules again.
    Change-Id: Id914ef7c8995915be38ba453b1c3229dec101974

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
-PYTHON=desres-python/2.7.15-05c7
-PYTHON3=desres-python/3.6.6-01c7
+PYTHON=desres-python/2.7.15-06c7
+PYTHON3=desres-python/3.6.6-02c7
 YAS=yas/0.73-beta-c7
 
 garden prepend-path PYTHONPATH $(dirname $0)/../lib/python
