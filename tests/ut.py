fdeff2e2a0cf7152a1ed278e4b674cfcb9626903
Justin Gullingsrud <gullingj@deshawresearch.com>
2018-09-13 15:11:23 -0400

    Unit test for legacy ion.
    Change-Id: I8ba52ce11bb5b4bc23574270c0d7c673fa844428

--- tests/ut.py
+++ tests/ut.py
@@ -602,6 +602,13 @@ class AtomselCoverage(unittest.TestCase):
         with self.assertRaises(RuntimeError):
             self.mol.select('')
 
+    def testIons(self):
+        m=msys.CreateSystem()
+        a=m.addAtom()
+        for resname in 'Cl-', 'K+':
+            a.residue.name=resname
+            self.assertEqual(len(m.select('legacy_ion')), 1, 'failed for %s' % resname)
+
     def testAllBoolean(self):
         heavy = [a.id for a in self.mol.atoms if a.atomic_number > 1]
         self.check('atomicnumber > 1', heavy)
