70921349472a61a8ee23b16826444a891327c873
Justin Gullingsrud <gullingj@deshawresearch.com>
2019-04-08 12:09:42 -0400

    Use python3 as executable in tools.
    Change-Id: I2ccca63ad7c3905342b287799fd0d9295ee2abde

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
 PYTHON=desres-python/2.7.15-08c7
 PYTHON3=desres-python/3.7.2-08c7
-YAS=yas/0.130c7
+YAS=yas/0.136c7
 
 garden prepend-path PYTHONPATH $(dirname $0)/../lib/python
-garden load $PYTHON/bin
-garden load $YAS/lib-python
+garden load $PYTHON3/bin
+garden load $YAS/lib-python37
 
