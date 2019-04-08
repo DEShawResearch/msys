3dfd831c88617df1a4afa0a9b8a4a45d561eb90c
Justin Gullingsrud <gullingj@deshawresearch.com>
2019-04-08 11:55:33 -0400

    Use mathjax extension.
    Change-Id: Ibec715b3c04f9119f49f012a0e5bc638b4e9944a

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -35,6 +35,7 @@ else:
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
               'sphinx.ext.napoleon',
+              'sphinx.ext.mathjax',
               ]
 
 # Add any paths that contain templates here, relative to this directory.
