dbb11f8b3b4e524fddc03b01b667f9852b4dcfb3
Justin Gullingsrud <gullingj@deshawresearch.com>
2019-07-18 11:56:51 -0400

    Bump python version.
    Change-Id: I2f912330ae076e1f19b8d218f9fc5292f21c1270

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 PYTHON=desres-python/2.7.15-10c7
-PYTHON3=desres-python/3.7.3-03c7
-YAS=yas/0.139c7
+PYTHON3=desres-python/3.7.4-01c7
+YAS=yas/0.143c7
 
 garden prepend-path PYTHONPATH $(dirname $0)/../lib/python
 garden load $PYTHON3/bin
