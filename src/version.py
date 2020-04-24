fc7957400187ae8082988fc0c90dd07cab469e86
Lev Iserovich <iserovil@deshawresearch.com>
2020-04-24 13:43:10 -0400

    Bump version

--- src/version.py
+++ src/version.py
@@ -2,7 +2,7 @@
 
 from __future__ import print_function
 
-version = "1.7.294"
+version = "1.7.295"
 major_version, minor_version, micro_version = [int(_x) for _x in version.split('.')]
 hexversion = (major_version << 16) | \
              (minor_version << 8)  | \
