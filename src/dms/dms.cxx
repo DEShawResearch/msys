DEShawResearch/msys

commit 3db9ed94893db61e801ef0282f54301cd891b5c2

Eliminate quadratic reallocation in DMS writes.

src/dms/dms.cxx
@@ -88,6 +88,7 @@ namespace {
         dms_file *dms = (dms_file *)file;
         sqlite3_int64 last=offset+iAmt;
         if (dms->size < last) {
+            last *= 1.5;
             dms->contents = (char *)realloc(dms->contents, last);
             dms->size = last;
         }

commit de7bc83adbe8c44610e2130119c5bd3d0c3ba7d8

Don't write extraneous data at end of DMS.
Fixes bug introduced in 3db9ed948

src/dms/dms.cxx
@@ -22,6 +22,7 @@ namespace {
     struct dms_file : sqlite3_file {
         char * contents;
         sqlite3_int64 size;
+        sqlite3_int64 capacity;
         char * path;
 
         void write() const {
@@ -87,11 +88,11 @@ namespace {
     int dms_xWrite(sqlite3_file*file, const void*pBuf, int iAmt, sqlite3_int64 offset) {
         dms_file *dms = (dms_file *)file;
         sqlite3_int64 last=offset+iAmt;
-        if (dms->size < last) {
-            last *= 1.5;
-            dms->contents = (char *)realloc(dms->contents, last);
-            dms->size = last;
+        if (dms->capacity < last) {
+            dms->capacity = last * 1.5;
+            dms->contents = (char *)realloc(dms->contents, dms->capacity);
         }
+        dms->size = std::max(dms->size, offset + iAmt);
         memcpy(dms->contents+offset, pBuf, iAmt);
         return SQLITE_OK;
     }

tests/test_ut.py
@@ -2924,10 +2924,7 @@ class Main(unittest.TestCase):
             s = fp.read()
         msys.LoadDMS(buffer=s)
 
-    @unittest.skipIf(True, "unsupported feature")
     def testLoadDmsMany(self):
-        # FIXME this broken relatively recently, but concatenated DMS files
-        # aren't actually used by anyone, are they?
         tmp = tempfile.NamedTemporaryFile(suffix=".dms")
         for mol in (
             msys.Load("tests/files/%s" % m)