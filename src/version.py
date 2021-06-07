0355285a21de09edd5482caf901460a455a062b3
Justin Gullingsrud <gullingj@deshawresearch.com>
2021-06-07 07:31:28 -0400

    Release notes.

--- src/version.py
+++ src/version.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-version = "1.7.332"
+version = "1.7.333"
 major_version, minor_version, micro_version = [int(_x) for _x in version.split('.')]
 hexversion = (major_version << 16) | \
              (minor_version << 8)  | \
