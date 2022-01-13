afaf566df269ffbc6363d7557f00547bbfea964a
Lev Iserovich <iserovil@deshawresearch.com>
2022-01-13 15:26:24 -0500

    Parallelize dtr-validate
    dtr-validate can be slow on compressed trajectories, since decompression
    is CPU-heavy
    Parallelize the slow frame read and decompress thread to speed it up
    New '--parallelism' command line option controls number of concurrent frames
    to read, and defaults to 1/2 the number of cores
    
    Also fix the progress display
    
    DESRESInfra#49930
    
    Change-Id: I6828f13d0880093cef3d51d5a4837e9d5d4c7181

--- tools/validate/dtr.py
+++ tools/validate/dtr.py
@@ -7,10 +7,13 @@ position and box data.
 import sys, os, fnmatch
 from msys import molfile
 import numpy
+import math
+import threading
+import multiprocessing
 
 
 class Progress(object):
-    def __init__(self, total, dots=50, nums=10):
+    def __init__(self, total, dots=50, nums=11):
         points = list(map(int, numpy.linspace(1, total, dots)))
         progs = list(map(int, numpy.linspace(0, 100, dots)))
         self.dots = dict(zip(points, progs))
@@ -28,6 +31,7 @@ class Progress(object):
             sys.stderr.write(".")
         if i == self.total:
             sys.stderr.write("\n")
+        sys.stderr.flush()
 
 
 def parse_args():
@@ -56,14 +60,28 @@ def parse_args():
         default=False,
         help="show parseable output compatible with d_validate",
     )
+    parser.add_argument(
+        "--parallelism",
+        type=int,
+        default=int(math.ceil(multiprocessing.cpu_count()/2.0)),   # use 1/2 the cores, round up to never get 0
+        help="Number of frames to process in parallel",
+    )
 
     return parser.parse_args()
 
+class FrameLoadThread(threading.Thread):
+    def __init__(self, reader, frameno):
+        super().__init__()
+        self.reader = reader
+        self.frameno = frameno
+
+    def run(self):
+        self.frame = self.reader.frame(self.frameno)
 
 def main():
     args = parse_args()
     path = args.input_path
-    r = molfile.DtrReader(path, sequential=True)
+    r = molfile.DtrReader(path, sequential=False)    # multi-threaded - need to re-open on each access
 
     if os.path.isdir(path):
         total_framebytes = 0
@@ -87,35 +105,45 @@ def main():
     assert nframes > 0, "'%s' contains %d frames." % (path, nframes)
     if args.progress:
         progress = Progress(nframes)
-    for i in range(nframes):
-        frame = r.frame(i)
-        if not args.energy:
-            assert frame.pos is not None, "'%s' is missing pos data at frame %d" % (
-                path,
-                i,
-            )
-            assert frame.box is not None, "'%s' is missing pos data at frame %d" % (
-                path,
-                i,
-            )
-        if args.progress:
-            progress(i + 1)
 
-        if i == 0:
-            first_frame_time = frame.time
-        else:
-            delta_t = frame.time - last_frame_time
-            if i > 1:
-                change_delta_t = abs(last_delta_t - delta_t)
-                if args.checkpoint == False:
-                    assert change_delta_t < 0.02, "delta_t changed from %f to %f" % (
-                        last_delta_t,
-                        delta_t,
-                    )
+    for start in range(0, nframes, args.parallelism):
+        # Read up to args.parallelism frames at a time in separate threads
+        frame_reader_threads = []
+        for j in range(start, nframes)[:args.parallelism]:
+            frame_reader_threads.append(FrameLoadThread(r, j))
+            frame_reader_threads[-1].start()
+        for j in range(len(frame_reader_threads)):
+            # Wait till frame read is finished, then grab the completed frame
+            frame_reader_threads[j].join()
+            frame = frame_reader_threads[j].frame
+            i = start + j
+            if not args.energy:
+                assert frame.pos is not None, "'%s' is missing pos data at frame %d" % (
+                    path,
+                    i,
+                )
+                assert frame.box is not None, "'%s' is missing pos data at frame %d" % (
+                    path,
+                    i,
+                )
+            if args.progress:
+                progress(i + 1)
 
-            last_delta_t = delta_t
+            if i == 0:
+                first_frame_time = frame.time
+            else:
+                delta_t = frame.time - last_frame_time
+                if i > 1:
+                    change_delta_t = abs(last_delta_t - delta_t)
+                    if args.checkpoint == False:
+                        assert change_delta_t < 0.02, "delta_t changed from %f to %f" % (
+                            last_delta_t,
+                            delta_t,
+                        )
 
-        last_frame_time = frame.time
+                last_delta_t = delta_t
+
+            last_frame_time = frame.time
 
     #
     # Spit out some output that is compatible with d_validate, which
