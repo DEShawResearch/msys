bfe188ab28a619279ab27f7932132e8ae346a834
Jacob Pessin <pessin@deshawresearch.com>
2022-07-25 19:44:23 -0400

    bump env.sh and GIH for gcc11.1 and desres-python-3.10 dev

--- env.sh
+++ env.sh
@@ -1,7 +1,8 @@
-PYTHON3=desres-python/3.7.7-06c7
-YAS=yas/0.163c7
+PYTHON3=desres-python/3.10.5dev1-04c7
+YAS=yas/0.189c7
+# current yas, as of, is yas/0.189c7
 
 garden prepend-path PYTHONPATH $(dirname $0)/../lib/python
 garden load $PYTHON3/bin
 garden load $YAS/lib-python37
-
+garden load $YAS/lib-python310
