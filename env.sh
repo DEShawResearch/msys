4826a520da8d782fa1826f6bfb6ac44e48cbf368
Jacob Pessin <pessin@deshawresearch.com>
2022-08-03 18:35:17 -0400

    bump for 3.10, prereq PyBind11
    Change-Id: Ib6daca57527d21818a382115ae8de62f10c5ab1a

--- env.sh
+++ env.sh
@@ -1,6 +1,5 @@
-PYTHON3=desres-python/3.10.5dev1-04c7
-YAS=yas/0.189c7
-# current yas, as of, is yas/0.189c7
+PYTHON3=desres-python/3.10.5-04c7
+YAS=yas/0.190c7
 
 garden prepend-path PYTHONPATH $(dirname $0)/../lib/python
 garden load $PYTHON3/bin
